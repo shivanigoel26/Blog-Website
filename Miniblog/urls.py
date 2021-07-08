54903984deb656d556f997e7510e17f1c073ca7e
shivanigoel26 <shivanigoel9876@gmail.com>
2021-07-08 23:51:34 +0530

    update

--- Miniblog/urls.py
+++ Miniblog/urls.py
@@ -23,4 +23,11 @@ urlpatterns = [
     path('', views.home),
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('logout/',views.user_logout, name='logout'),
+    path('signup/',views.user_signup, name='signup'),
+    path('login/',views.user_login, name='login'),
+    path('addpost/', views.add_post, name='addpost'),
+    path('updatepost/<int:id>/', views.update_post, name='updatepost'),
+    path('delete/<int:id>/', views.delete_post, name='deletepost'),
 ]
